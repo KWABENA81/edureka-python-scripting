f82f1f9fcd13fc4446a044f1847eecfc4adf8dd0
Kwabena S <kwabenasefa@hotmail.com>
2022-09-23 08:41:17 -0400

    ...commit

---
+++ python_script_cs/main_Q2.py
@@ -0,0 +1,11 @@
+def print_hi(name):
+    var = input("enter word sequence:")
+    var2 = sorted(var.split())
+    for x in var2:
+        print(x)
+
+
+if __name__ == '__main__':
+    print_hi('Solomon S A')
+
+# See PyCharm help at https://www.jetbrain
